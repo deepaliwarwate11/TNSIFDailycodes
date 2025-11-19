62add1414646f5e971c91e9a240d39d5d96cf286
deepaliwarwate11 <dipaliwarwate@gmail.com>
2025-11-19 21:08:01 +0530

    initial commit

---
+++ dayten/src/com/dayten/Arraysdemo/MLArray.java
@@ -0,0 +1,17 @@
+package com.dayten.Arraysdemo;
+
+public class MLArray {
+	
+static void printArray(int c[][]) {           
+		
+		for(int i=0; i<c.length; i++)
+		{
+			for(int j=0; j< c[i].length; j++)
+			{
+				System.out.print(c[i][j]+ " ");
+			}
+			System.out.println();
+		}
+	}
+
+}
