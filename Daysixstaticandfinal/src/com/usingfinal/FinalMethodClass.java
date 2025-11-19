f32d23e3ab55518f50c10ba6d7d632e618b29367
deepaliwarwate11 <dipaliwarwate@gmail.com>
2025-11-19 20:46:19 +0530

    initial commit

---
+++ Daysixstaticandfinal/src/com/usingfinal/FinalMethodClass.java
@@ -0,0 +1,18 @@
+package com.usingfinal;
+
+public class FinalMethodClass {
+	
+
+	final int a = 50;
+	
+	final void show()
+	{
+		System.out.println("Value of a: "+a);
+	}
+
+//	final void show()
+//	{
+//		System.out.println("show Method");
+//	}
+
+}
